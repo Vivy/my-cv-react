493add1220b53d90107075020e1e19e26657ba8f
Vivy <Tomitaviorel@yahoo.com>
2022-10-11 15:36:35 +0300

    feat: I made the Welcome page for my CV, added the divider component, changed my-cv conmponent

--- src/component/my-cv/my-cv.jsx
+++ src/component/my-cv/my-cv.jsx
@@ -1,28 +1,12 @@
-import { Box, Text, Button, Header, Welcome, Flex, Reset } from '..';
+import { Box, Text, Button, Header, Welcome, Flex, Reset, Divider } from '..';
 
 const MyCV = () => {
   return (
     <div>
       <Reset />
       <Header>Welcome to my CV</Header>
-      <Flex vertical align='center' justify='center'>
-        <Button
-          href='https://www.youtube.com/watch?v=kj1hyIxTpLM'
-          title='Let`s relax with music'
-          color='transparent'
-        >
-          😃
-        </Button>
-        <Welcome />
-        <Button
-          href='C:\Users\LENOVO\Desktop\CV`s\'
-          title='Let`s meet ME'
-          color='funGreen'
-        >
-          Download
-        </Button>
-      </Flex>
-      <Header />
+      <Welcome />
+      {/* <Divider height={16}/> */}
     </div>
   );
 };
--- src/component/welcome/welcome.jsx
+++ src/component/welcome/welcome.jsx
@@ -1,6 +1,7 @@
 import * as S from './welcome.style';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Button, Header, Divider, Flex, Box } from '..';
 
 const Welcome = () => {
   const [summary, update] = useState({});
@@ -14,10 +15,36 @@ const Welcome = () => {
     bub();
   }, []);
   return (
-    <>
-      <S.Welcome>{summary.nameTitle} </S.Welcome>
+    <Flex vertical align='center' justify='center'>
+      <Button
+        href='https://www.youtube.com/watch?v=kj1hyIxTpLM'
+        title='Let`s relax with music'
+        color='transparent'
+      >
+        😃
+      </Button>
+      <S.Welcome>{summary.nameTitle}</S.Welcome>
       <S.Welcome>{summary.jobTitle}</S.Welcome>
-    </>
+      <Button
+        href='C:\Users\LENOVO\Desktop\CV`s\'
+        title='Let`s meet ME'
+        color='funGreen'
+      >{summary.downloadButtonLabel}
+      </Button>
+      <Divider height={32} />
+      <Header font={16} color='funOrange'>
+        Continue reading so you can get to know how I can help you help me help
+        you so everytone gets helped
+      </Header>
+      <Divider height={16} />
+        <Box
+          padding={8}
+          gap={8}
+          aboutTitle={summary.aboutTitle}
+          descriptionList={summary.descriptionList}
+        ></Box>
+      <Header />
+    </Flex>
   );
 };
 
